4c57548b78eb544fe38751b6028a4f856b618c0c
Mike Grier <micgrier@microsoft.com>
2025-07-27 21:13:47 -0400

    Add tests for m::raw_allocator, fix some type issues

--- src/libraries/memory/test/test_raw_allocator.cpp
+++ src/libraries/memory/test/test_raw_allocator.cpp
@@ -14,27 +14,25 @@
 using namespace std::string_view_literals;
 
 std::array const byte_array_abc{std::byte{'a'}, std::byte{'b'}, std::byte{'c'}};
-auto const              byte_span_abc = std::span(byte_array_abc);
+// auto const       byte_span_abc = std::span(byte_array_abc);
 
-TEST(MemoryUniqueSpan, ByteSpan1)
+TEST(MemoryRawAllocator, ByteSpanUnconstructed)
 {
-    auto const us = m::unique_span(byte_span_abc);
-
-    EXPECT_EQ(us.size(), byte_span_abc.size());
-    EXPECT_TRUE(std::ranges::equal(byte_span_abc, us));
+    m::raw_allocator<std::byte> ra(byte_array_abc.size());
+    EXPECT_EQ(ra.size(), byte_array_abc.size());
+    EXPECT_EQ(ra.constructed(), 0);
 }
 
-TEST(MemoryUniqueSpan, ByteSpan2)
+TEST(MemoryRawAllocator, ByteSpanConstructed)
 {
-    auto const us = m::unique_span(byte_array_abc);
-
-    EXPECT_EQ(us.size(), byte_array_abc.size());
-    EXPECT_TRUE(std::ranges::equal(byte_array_abc, us));
+    m::raw_allocator<std::byte> ra(byte_array_abc.size());
+    ra.default_construct();
+    EXPECT_EQ(ra.size(), byte_array_abc.size());
+    EXPECT_EQ(ra.constructed(), ra.size());
 }
 
 namespace
 {
-
     // owc == object_with_constructor
     //
     // Silly type just there to let us count number of various operators executing.
@@ -42,7 +40,8 @@ namespace
 
     struct owc
     {
-        owc(std::byte b): m_b(b) { ms_dccnt++; }
+        owc(): m_b{} { ms_dccnt++; }
+        owc(std::byte b): m_b(b) { ms_bccnt++; }
         owc(owc const& other): m_b(other.m_b) { ms_cccnt++; }
         owc(owc&& other): m_b(std::byte{0})
         {
@@ -70,6 +69,7 @@ namespace
         }
 
         static inline std::atomic<uintmax_t> ms_dccnt{0};  // default constructor count
+        static inline std::atomic<uintmax_t> ms_bccnt{0};  // byte constructor count
         static inline std::atomic<uintmax_t> ms_cccnt{0};  // copy constructor count
         static inline std::atomic<uintmax_t> ms_mvccnt{0}; // move constructor count
         static inline std::atomic<uintmax_t> ms_aocnt{0};  // assignment operator count
@@ -79,17 +79,20 @@ namespace
         std::byte m_b;
     };
 
+    #if 0
     constexpr bool
     operator==(owc const& l, owc const& r) noexcept
     {
         return l.m_b == r.m_b;
     }
+    #endif
 
     struct owc_stats
     {
         owc_stats() noexcept
         {
             m_dccnt  = owc::ms_dccnt;
+            m_bccnt  = owc::ms_bccnt;
             m_cccnt  = owc::ms_cccnt;
             m_mvccnt = owc::ms_mvccnt;
             m_aocnt  = owc::ms_aocnt;
@@ -98,6 +101,7 @@ namespace
         }
 
         uintmax_t m_dccnt;
+        uintmax_t m_bccnt;
         uintmax_t m_cccnt;
         uintmax_t m_mvccnt;
         uintmax_t m_aocnt;
@@ -109,6 +113,7 @@ namespace
     {
         constexpr owc_statdiff() noexcept:
             m_dccnt_delta{0},
+            m_bccnt_delta{0},
             m_cccnt_delta{0},
             m_mvccnt_delta{0},
             m_aocnt_delta{0},
@@ -118,6 +123,7 @@ namespace
 
         constexpr owc_statdiff(owc_statdiff const& other) noexcept:
             m_dccnt_delta{other.m_dccnt_delta},
+            m_bccnt_delta{other.m_bccnt_delta},
             m_cccnt_delta{other.m_cccnt_delta},
             m_mvccnt_delta{other.m_mvccnt_delta},
             m_aocnt_delta{other.m_aocnt_delta},
@@ -128,6 +134,7 @@ namespace
         constexpr owc_statdiff(owc_stats const& first, owc_stats const& second) noexcept
         {
             m_dccnt_delta  = second.m_dccnt - first.m_dccnt;
+            m_bccnt_delta  = second.m_bccnt - first.m_bccnt;
             m_cccnt_delta  = second.m_cccnt - first.m_cccnt;
             m_mvccnt_delta = second.m_mvccnt - first.m_mvccnt;
             m_aocnt_delta  = second.m_aocnt - first.m_aocnt;
@@ -139,6 +146,7 @@ namespace
         operator=(owc_statdiff const& other) noexcept
         {
             m_dccnt_delta  = other.m_dccnt_delta;
+            m_bccnt_delta  = other.m_bccnt_delta;
             m_cccnt_delta  = other.m_cccnt_delta;
             m_mvccnt_delta = other.m_mvccnt_delta;
             m_aocnt_delta  = other.m_aocnt_delta;
@@ -148,6 +156,7 @@ namespace
         }
 
         intmax_t m_dccnt_delta;
+        intmax_t m_bccnt_delta;
         intmax_t m_cccnt_delta;
         intmax_t m_mvccnt_delta;
         intmax_t m_aocnt_delta;
@@ -160,63 +169,100 @@ namespace
     {
         return owc_statdiff(r, l);
     }
+
 } // namespace
 
-TEST(MemoryUniqueSpan, CountOps1)
+std::array const owc_array{owc(std::byte{'a'}), owc(std::byte{'b'}), owc(std::byte{'c'})};
+
+TEST(MemoryRawAllocator, CountOpsUnconstructed)
 {
-    owc_statdiff     diff1;
-    owc_statdiff     diff2;
-    owc_statdiff     diff3;
-    owc_statdiff     diff4;
-    owc_stats        stats1;
-    std::array const owc_array{owc(std::byte{'a'}), owc(std::byte{'b'}), owc(std::byte{'c'})};
-    owc_stats        stats2;
-    auto             us = m::unique_span(owc_array);
-    owc_stats        stats3;
-    auto const       s = us.span();
-    owc_stats        stats4;
+    owc_stats             stats1;
+    m::raw_allocator<owc> ra(owc_array.size());
+    owc_stats             stats2;
+    EXPECT_EQ(ra.size(), owc_array.size());
+    EXPECT_EQ(ra.constructed(), 0);
 
-    EXPECT_TRUE(std::ranges::equal(owc_array, us));
-    EXPECT_TRUE(std::ranges::equal(owc_array, s));
+    //
+    // No construction, so no deltas expected
+    //
+    auto const diff1 = stats2 - stats1;
 
-    diff1 = stats2 - stats1;
-
-    std::println("diff1: dccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
+    std::println("diff1: dccnt: {} bccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
                  diff1.m_dccnt_delta,
+                 diff1.m_bccnt_delta,
                  diff1.m_cccnt_delta,
                  diff1.m_mvccnt_delta,
                  diff1.m_aocnt_delta,
                  diff1.m_mvocnt_delta,
                  diff1.m_dcnt_delta);
 
-    EXPECT_EQ(diff1.m_dccnt_delta, 3);
+    EXPECT_EQ(diff1.m_dccnt_delta, 0);
+    EXPECT_EQ(diff1.m_bccnt_delta, 0);
+    EXPECT_EQ(diff1.m_cccnt_delta, 0);
+    EXPECT_EQ(diff1.m_mvccnt_delta, 0);
+    EXPECT_EQ(diff1.m_aocnt_delta, 0);
+    EXPECT_EQ(diff1.m_mvocnt_delta, 0);
+    EXPECT_EQ(diff1.m_dcnt_delta, 0);
+}
+
+TEST(MemoryRawAllocator, CountOpsConstructed)
+{
+    owc_stats             stats1;
+    m::raw_allocator<owc> ra(owc_array.size());
+    owc_stats             stats2;
+    EXPECT_EQ(ra.size(), owc_array.size());
+    EXPECT_EQ(ra.constructed(), 0);
+    ra.default_construct();
+    owc_stats stats3;
+    ra.reset();
+    owc_stats stats4;
+
+    //
+    // No construction, so no deltas expected
+    //
+    auto const diff1 = stats2 - stats1;
+
+    std::println("diff1: dccnt: {} bccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
+                 diff1.m_dccnt_delta,
+                 diff1.m_bccnt_delta,
+                 diff1.m_cccnt_delta,
+                 diff1.m_mvccnt_delta,
+                 diff1.m_aocnt_delta,
+                 diff1.m_mvocnt_delta,
+                 diff1.m_dcnt_delta);
+
+    EXPECT_EQ(diff1.m_dccnt_delta, 0);
+    EXPECT_EQ(diff1.m_bccnt_delta, 0);
     EXPECT_EQ(diff1.m_cccnt_delta, 0);
     EXPECT_EQ(diff1.m_mvccnt_delta, 0);
     EXPECT_EQ(diff1.m_aocnt_delta, 0);
     EXPECT_EQ(diff1.m_mvocnt_delta, 0);
     EXPECT_EQ(diff1.m_dcnt_delta, 0);
 
-    diff2 = stats3 - stats2;
+    auto const diff2 = stats3 - stats2;
 
-    std::println("diff2: dccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
+    std::println("diff2: dccnt: {} bccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
                  diff2.m_dccnt_delta,
+                 diff2.m_bccnt_delta,
                  diff2.m_cccnt_delta,
                  diff2.m_mvccnt_delta,
                  diff2.m_aocnt_delta,
                  diff2.m_mvocnt_delta,
                  diff2.m_dcnt_delta);
 
-    EXPECT_EQ(diff2.m_dccnt_delta, 0);
-    EXPECT_EQ(diff2.m_cccnt_delta, 3);
+    EXPECT_EQ(diff2.m_dccnt_delta, 3);
+    EXPECT_EQ(diff2.m_bccnt_delta, 0);
+    EXPECT_EQ(diff2.m_cccnt_delta, 0);
     EXPECT_EQ(diff2.m_mvccnt_delta, 0);
     EXPECT_EQ(diff2.m_aocnt_delta, 0);
     EXPECT_EQ(diff2.m_mvocnt_delta, 0);
     EXPECT_EQ(diff2.m_dcnt_delta, 0);
 
-    diff3 = stats4 - stats3;
+    auto const diff3 = stats4 - stats3;
 
-    std::println("diff3: dccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
+    std::println("diff3: dccnt: {} bccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
                  diff3.m_dccnt_delta,
+                 diff3.m_bccnt_delta,
                  diff3.m_cccnt_delta,
                  diff3.m_mvccnt_delta,
                  diff3.m_aocnt_delta,
@@ -224,31 +270,10 @@ TEST(MemoryUniqueSpan, CountOps1)
                  diff3.m_dcnt_delta);
 
     EXPECT_EQ(diff3.m_dccnt_delta, 0);
+    EXPECT_EQ(diff3.m_bccnt_delta, 0);
     EXPECT_EQ(diff3.m_cccnt_delta, 0);
     EXPECT_EQ(diff3.m_mvccnt_delta, 0);
     EXPECT_EQ(diff3.m_aocnt_delta, 0);
     EXPECT_EQ(diff3.m_mvocnt_delta, 0);
-    EXPECT_EQ(diff3.m_dcnt_delta, 0);
-
-    us.reset();
-
-    owc_stats stats5;
-    diff4 = stats5 - stats4;
-
-    std::println("diff4: dccnt: {} cccnt: {} mvccnt: {} aocnt: {} mvocnt: {} dcnt: {}",
-                 diff4.m_dccnt_delta,
-                 diff4.m_cccnt_delta,
-                 diff4.m_mvccnt_delta,
-                 diff4.m_aocnt_delta,
-                 diff4.m_mvocnt_delta,
-                 diff4.m_dcnt_delta);
-
-    EXPECT_EQ(diff4.m_dccnt_delta, 0);
-    EXPECT_EQ(diff4.m_cccnt_delta, 0);
-    EXPECT_EQ(diff4.m_mvccnt_delta, 0);
-    EXPECT_EQ(diff4.m_aocnt_delta, 0);
-    EXPECT_EQ(diff4.m_mvocnt_delta, 0);
-    EXPECT_EQ(diff4.m_dcnt_delta, 3);
-
-    EXPECT_EQ(us.size(), 0);
+    EXPECT_EQ(diff3.m_dcnt_delta, 3);
 }

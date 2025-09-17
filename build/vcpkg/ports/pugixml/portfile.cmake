6a2fdb9ac8e8af03c3a4aa5e5b4e3565c120d828
Michael Grier <micgrier@microsoft.com>
2025-09-16 20:02:22 -0400

    Synchronize pugixml port with ADO client's

--- build/vcpkg/ports/pugixml/portfile.cmake
+++ build/vcpkg/ports/pugixml/portfile.cmake
@@ -4,6 +4,14 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 b8a70f1f230b0902b719346ce0a551eafe534f81262280dceeb92d5ad90ea4e635173e08e225bf66eb5f4724ac4568bd40dc923f184571f02502dac49bc0b7f5
     HEAD_REF master
+    PATCHES
+        0001-cmakelists-txt-publish-pugixml-cpp.patch
+        0002-oacr-warning-pugixml-cpp.patch
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        header-only PUGIXML_HEADER_ONLY
 )
 
 vcpkg_cmake_configure(
@@ -11,6 +19,7 @@ vcpkg_cmake_configure(
     OPTIONS
         -DPUGIXML_BUILD_TESTS=OFF
         -DPUGIXML_WCHAR_MODE=ON
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
